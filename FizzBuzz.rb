cbba202ad44661b1891704a9daf22ac68357aaf8
jkvoorhis <jhenna.voorhis@gmail.com>
2013-08-28 18:39:31 -0700

    This is my solution to the FizzBuzz problem
    The FizzBuzz problem:
    Write a program that prints the numbers from 1 to 100. But for
    multiples of three print "Fizz" instead of the number and for the
    multiples of five print "Buzz". For numbers which are multiples of both
    three and five print "FizzBuzz".

---
+++ FizzBuzz.rb
@@ -0,0 +1,13 @@
+#Jhenna Voorhis
+#solution to the FizzBuzz problem
+1.upto(100){|number| 
+  if number%3 == 0 && number%5 ==0
+    puts "FizzBuzz"
+  elsif number%5 == 0
+    puts "Buzz"
+  elsif number%3 == 0 
+    puts "Fizz"
+  else
+    puts number
+  end
+}
